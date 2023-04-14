5bad77f1fbe6e8d531fe4c3ddb2bf6ae4475646b
SUVENDU2000 <suvendukumars19@gmail.com>
2023-04-14 22:04:10 +0530

    initial commit

---
+++ Confirm Seat/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Confirm Seat
+//
+//  Created by Shasanka Sahoo on 27/03/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
--- Confirm Seat/ViewController.swift
+++ Confirm Seat/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userIDOutlet: UITextField!
+    @IBOutlet weak var passwordOutlet: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func didTappedLoginButton(_ sender: UIButton) {
+        self.checkCredential()
+    }
+    
+    private func checkCredential() {
+        guard let userID = userIDOutlet.text,
+              !userID.isEmpty,
+              let passwordField = passwordOutlet.text,
+              !passwordField.isEmpty else {
+            print("Field Empty")
+            return
+        }
+        if userID == userData.userName {
+            self.performSegue(withIdentifier: "showLoginSuccessful", sender: nil)
+        }
+    }
 }
 
+struct userData {
+    var userName: String
+    var email: String
+}
